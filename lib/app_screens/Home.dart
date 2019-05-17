8c339a7df28bc723718ceac002ecc6d3c9ab846e
depth <bardaabdelaziz@gmail.com>
2019-05-17 10:26:18 +0000

    add color and text

--- lib/app_screens/Home.dart
+++ lib/app_screens/Home.dart
@@ -8,13 +8,13 @@ class Home extends StatelessWidget {
     return Center( // Center Widget
         child: Container(                            // Put your Container inside Center Widget to Control its size
           alignment: Alignment.center,
-          color: Colors.,
+          color: Colors.blueGrey,
           //width: 200.0,
           //height: 100.0,
           //margin: EdgeInsets.only(top: 40.0, bottom: 60.0, right: 20.0, left: 20.0), // Set Margin to container
           //padding: EdgeInsets.all(5.0),
           child: Text(
-            "Bricol",
+            "Bricol App",
             textDirection: TextDirection.ltr,
             style: TextStyle(                       // Style Text Widget
                 decoration: TextDecoration.none,    // to remove underline
